f2ba8c8a344c388b16a678d25fea30cb9f80d8e7
laiot <carmelo.sarta.main@gmail.com>
2019-10-08 15:44:11 +0200

    Issue with __init__ files.

--- Randomness/__init__.py
+++ Randomness/__init__.py
@@ -1,6 +1,8 @@
 from Randomness.MarkovChain import MarkovChain
+import os
 
-config = open("config.txt", "r")
+dirname = os.path.dirname(__file__)
+config = open(os.path.join(dirname, 'Configuration Files/config.txt'), "r")
 probs_file_path = config.readline().split()[1]
 first_page = config.readline().split()[1]
 final_pages = [page for page in config.readline().split()[1:]]

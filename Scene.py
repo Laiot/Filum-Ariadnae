370d705ae4eb4555869a6537d56860544f65eef9
laiot <carmelo.sarta.main@gmail.com>
2019-10-12 22:48:15 +0200

    Scene added

--- Scene.py
+++ Scene.py
@@ -1,9 +1,14 @@
+from People.Support import Support
+
 """
     Class representing the single scene.
 """
 
 
 class Scene(object):
-    def __init__(self, public_flag, npc):
+    def __init__(self, public_flag, person):
         self.public = public_flag
-        self.npc = npc
+        self.person = person
+        if isinstance(person, Support):
+            self.isSupport = True
+        # TODO define Markov Chain supported by actions database with probability Matrix

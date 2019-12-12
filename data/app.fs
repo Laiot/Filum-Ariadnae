b457e71fc2739211ed198279030bfa6bed8655ec
Carmelo Sarta <carmelo.sarta.main@gmail.com>
2019-12-12 08:27:35 +0100

    Add files via upload

---
+++ data/app.fs
@@ -0,0 +1,11 @@
+FS21Œõ5∆qùÃ       ó      initial database creation        Œõ5∆qùÃ                        =Äcpersistent.mapping
+PersistentMapping
+q.Ä}qUdataq}qs.       óŒõ5«-ß›                     Œõ5«-ß›       4       £        ŸÄcpersistent.mapping
+PersistentMapping
+q.Ä}qUdataq}qX$   8700f8cc-df88-4638-8053-66e396ec6cb0q}q(Uq1qX   4Uq3qX   2Uq2q	X   5Uq5q
+X   NUq4qX   3Uq7qX   YUq6q
+X   YUgenderqX   MUageqX   43qUfreetextqX   Commento di prova di lunghezza superiore alla media, qui il nostro soggetto di test puo' inserire piu' o meno quello che vuole.qU	residenceqX   MiCityqUpathqX   books/AriannaELucaqUhistoryqX   .0.1.3.4.5.7.16.17.20.21quss.      Œ¬Ñî¯‚                      Œ¬Ñî¯‚        ∫      ≈        —Äcpersistent.mapping
+PersistentMapping
+q.Ä}qUdataq}q(X$   8700f8cc-df88-4638-8053-66e396ec6cb0q}q(Uq1qX   4Uq3qX   2Uq2q	X   5Uq5q
+X   NUq4qX   3Uq7qX   YUq6q
+X   YUgenderqX   MUageqX   43qUfreetextqX   Commento di prova di lunghezza superiore alla media, qui il nostro soggetto di test puo' inserire piu' o meno quello che vuole.qU	residenceqX   MiCityqUpathqX   books/AriannaELucaqUhistoryqX   .0.1.3.4.5.7.16.17.20.21quX$   5f7253da-a237-4949-b573-e28b175fba3aq}q(Uq1qX   3Uq3qX   3Uq2qX   3Uq5qUNAqUq4q X   3Uq7q!hUq6q"hUgenderq#hUageq$X    Ufreetextq%X    U	residenceq&hUpathq'X   books/AriannaELucaq(Uhistoryq)X   .0.1.2.4.6.11.21q*uus.      
